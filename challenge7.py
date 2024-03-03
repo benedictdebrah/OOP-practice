32b10ec6d551c5fe6425912a8bb20d4c16e952a8
benedictdebrah <benedictdebrah.o@gmail.com>
2024-03-03 23:37:36 +0000

    changes on my own

--- challenge7.py
+++ challenge7.py
@@ -1,34 +1,41 @@
-class Account:  # parent class
-    def __init__(self, title=None, balance=0):
+class Account: #parent class
+
+    #initializing the class
+    def __init__(self,title = None, balance = 0):
         self.title = title
         self.balance = balance
-    # withdrawal method subtracts the amount from the balance
-
-    def withdrawal(self, amount):
-        self.balance = self.balance - amount
-    # deposit method adds the amount to the balance
-
-    def deposit(self, amount):
-        self.balance = self.balance + amount
-    # this method just returns the value of balance
-
+    
+    #method to view balance
     def getBalance(self):
         return self.balance
-
-
-class SavingsAccount(Account):
-    def __init__(self, title=None, balance=0, interestRate=0):
+    
+    #method to deposist money
+    def deposit(self, amount):
+        self.balance += amount
+        return self.balance
+    
+    #method to withdraw money
+    def withdraw(self, amount):
+        if amount > self.balance:
+            return 'Insufficient balance'
+        else:
+            self.balance -= amount
+            return self.balance
+        
+class SavingsAccount(Account): #child class
+    def __init__(self, title = None, balance = 0, interestRate = 0):
         super().__init__(title, balance)
         self.interestRate = interestRate
-    # computes interest amount using the interest rate
-
+    
+    #method to calculate interest
     def interestAmount(self):
-        return (self.balance * self.interestRate / 100)
+        return (self.interestRate * self.balance)/100
+    
 
-
+    
 obj1 = SavingsAccount("Steve", 5000, 10)
 print("Initial Balance:", obj1.getBalance())
-obj1.withdrawal(1000)
+obj1.withdraw(1000)
 print("Balance after withdrawal:", obj1.getBalance())
 obj1.deposit(500)
 print("Balance after deposit:", obj1.getBalance())
